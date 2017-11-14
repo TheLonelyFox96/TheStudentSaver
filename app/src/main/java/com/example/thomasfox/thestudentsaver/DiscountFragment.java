9a1a424abe03b64665c4cf0e324dfa4af0f3c0cd
Thomas Fox <thomas.j.fox96@icloud.com>
2017-11-14 20:28:12 +0000

    Initial Code Commit

---
+++ app/src/main/java/com/example/thomasfox/thestudentsaver/DiscountFragment.java
@@ -0,0 +1,24 @@
+package com.example.thomasfox.thestudentsaver;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by thomasfox on 12/11/2017.
+ */
+
+public class DiscountFragment extends Fragment{
+    View myview;
+    @Nullable
+    @Override
+
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myview = inflater.inflate(R.layout.activity_discounts,container,false);
+        return myview;
+    }
+}
---
+++ app/src/main/java/com/example/thomasfox/thestudentsaver/MapFragment.java
@@ -0,0 +1,97 @@
+package com.example.thomasfox.thestudentsaver;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.PermissionChecker;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by thomasfox on 12/11/2017.
+ */
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    MapView mMapView;
+    View mView;
+    GoogleMap mGoogleMap;
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final int INITIAL_REQUEST=1337;
+    private static final int LOCATION_REQUEST=INITIAL_REQUEST+3;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.activity_map, container, false);
+        return mView;
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
+        mMapView.onCreate(null);
+        mMapView.onResume();
+        mMapView.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        googleMap = googleMap;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        if (canAccessLocation()) {
+            googleMap.setMyLocationEnabled(true);
+        }
+        else {
+            requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+
+        }
+
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED == PermissionChecker.checkSelfPermission(getContext(), perm));
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
